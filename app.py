75d59b770d6c9f76a81403952dbba1840cb5ec01
orangain <orangain@gmail.com>
2014-12-18 23:38:05 +0900

    Remove codes for cache

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import asyncio
 
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
-import pylibmc
 
 from check import check, Crawlability
 
@@ -13,33 +12,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-
-MEMCACHIER_SERVERS = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
-MEMCACHIER_USERNAME = os.environ.get('MEMCACHIER_USERNAME', '')
-MEMCACHIER_PASSWORD = os.environ.get('MEMCACHIER_PASSWORD', '')
-
-CACHE_DISABLED = os.environ.get('CACHE_DISABLED') == '1'
-CACHE_TIMEOUT_SECONDS = 5 * 60
-
-mc = pylibmc.Client(MEMCACHIER_SERVERS, binary=True,
-                    username=MEMCACHIER_USERNAME, password=MEMCACHIER_PASSWORD,
-                    behaviors={"tcp_nodelay": True,
-                               "ketama": True,
-                               "no_block": True,})
-
-
-def _get_from_cache(bytes_url):
-    if CACHE_DISABLED:
-        return None
-
-    return mc.get(bytes_url)
-
-
-def _store_into_cache(bytes_url, c):
-    if CACHE_DISABLED:
-        return
-
-    mc.set(bytes_url, c, time=CACHE_TIMEOUT_SECONDS)
 
 
 @asyncio.coroutine
@@ -56,10 +28,7 @@ def handle_result(request):
     if len(bytes_url) > 250:
         return web.Response(status=400, body='url parameter is too long')
 
-    c = _get_from_cache(bytes_url)
-    if c is None:
-        c = yield from check(url)
-        _store_into_cache(bytes_url, c)
+    c = yield from check(url)
 
     template = env.get_template('result.html')
     return web.Response(body=template.render(c=c).encode('utf-8'))
